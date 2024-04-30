067e81eccb00c80fe17e60d0e65ff9423361670c
Yet Another Stupid Coder <117515380+WhyNeet@users.noreply.github.com>
2024-04-30 12:02:53 +0300

    fix(api): add missing relation module to community feature imports

--- apps/api/src/features/community/community-feature.module.ts
+++ apps/api/src/features/community/community-feature.module.ts
@@ -1,11 +1,12 @@
 import { Module } from "@nestjs/common";
 import { DataServicesModule } from "src/frameworks/data-services/data-services.module";
+import { RelationFeatureModule } from "../relation/relation-feature.module";
 import { UserFeatureModule } from "../user/user-feature.module";
 import { CommunityFactoryService } from "./community-factory.service";
 import { CommunityRepositoryService } from "./community-repository.service";
 
 @Module({
-	imports: [DataServicesModule, UserFeatureModule],
+	imports: [DataServicesModule, UserFeatureModule, RelationFeatureModule],
 	providers: [CommunityFactoryService, CommunityRepositoryService],
 	exports: [CommunityFactoryService, CommunityRepositoryService],
 })

b719397ffba019fbfc2c9ab74c1f9ab9cc08670f
Yet Another Stupid Coder <117515380+WhyNeet@users.noreply.github.com>
2024-05-02 18:26:30 +0300

    fix(api): add required properties to posts schema in milvus

--- apps/api/src/frameworks/vector-services/vector-storage-services/milvus/schema/posts.schema.ts
+++ apps/api/src/frameworks/vector-services/vector-storage-services/milvus/schema/posts.schema.ts
@@ -6,26 +6,30 @@ export const PostsSchema: FieldType[] = [
 		description: "ID from the database",
 		data_type: DataType.VarChar,
 		is_primary_key: true,
+		max_length: 24,
 	},
 	{
 		name: "title",
 		description: "Post title",
 		data_type: DataType.VarChar,
+		max_length: 300,
 	},
 	{
 		name: "vector",
 		description: "Title vector embedding",
 		data_type: DataType.FloatVector,
-		dim: 1024,
+		dim: 384,
 	},
 	{
 		name: "author",
 		description: "Author ID from the database",
 		data_type: DataType.VarChar,
+		max_length: 24,
 	},
 	{
 		name: "collection",
 		description: "Collection ID from the database",
 		data_type: DataType.VarChar,
+		max_length: 24,
 	},
 ];

f775bb7d985be6ad82d5cfcd469cb6f8c39f5a9e
Yet Another Stupid Coder <117515380+WhyNeet@users.noreply.github.com>
2024-05-06 22:09:06 +0300

    build(web): add ngrx dependency

--- apps/web/src/app/app.config.ts
+++ apps/web/src/app/app.config.ts
@@ -2,8 +2,9 @@ import { ApplicationConfig } from "@angular/core";
 import { provideRouter } from "@angular/router";
 
 import { provideClientHydration } from "@angular/platform-browser";
+import { provideStore } from "@ngrx/store";
 import { routes } from "./app.routes";
 
 export const appConfig: ApplicationConfig = {
-	providers: [provideRouter(routes), provideClientHydration()],
+	providers: [provideRouter(routes), provideClientHydration(), provideStore()],
 };

037df814361c336993f926fee43d2850d781116d
Yet Another Stupid Coder <117515380+WhyNeet@users.noreply.github.com>
2024-05-10 21:55:50 +0300

    feat(web): add skeleton component

---
+++ apps/web/src/app/components/ui/skeleton/skeleton.component.ts
@@ -0,0 +1,15 @@
+import { ChangeDetectionStrategy, Component, Input } from "@angular/core";
+
+@Component({
+	selector: "app-ui-skeleton",
+	standalone: true,
+	imports: [],
+	providers: [],
+	template: `
+        <div [classList]="['bg-text/10 animate-pulse', class].join(' ')"></div>
+    `,
+	changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SkeletonComponent {
+	@Input() class = "";
+}

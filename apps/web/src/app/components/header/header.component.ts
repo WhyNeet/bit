cc7f1e5d79e84597ce558b5f3cd39f433b1c229c
Yet Another Stupid Coder <117515380+WhyNeet@users.noreply.github.com>
2024-05-10 21:55:19 +0300

    style(web): add search element to header

--- apps/web/src/app/components/header/header.component.ts
+++ apps/web/src/app/components/header/header.component.ts
@@ -1,11 +1,14 @@
 import { ChangeDetectionStrategy, Component } from "@angular/core";
+import { NgIcon, provideIcons } from "@ng-icons/core";
+import { lucideSearch } from "@ng-icons/lucide";
 import { LogoComponent } from "../logo/logo.component";
 
 @Component({
 	selector: "app-header",
 	standalone: true,
-	imports: [LogoComponent],
+	imports: [LogoComponent, NgIcon],
 	providers: [],
+	viewProviders: [provideIcons({ lucideSearch })],
 	templateUrl: "./header.component.html",
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })

c9c9681551c2cdaf5c739c36510a40d82b47b03e
Yet Another Stupid Coder <117515380+WhyNeet@users.noreply.github.com>
2024-05-10 18:39:54 +0300

    fix(web): use BehaviorSubject in theme service

--- apps/web/src/app/features/theme/theme.service.ts
+++ apps/web/src/app/features/theme/theme.service.ts
@@ -1,28 +1,40 @@
 import { Injectable, afterNextRender } from "@angular/core";
+import { BehaviorSubject, Observable, Subject } from "rxjs";
 import { Theme } from "./theme.interface";
 
 @Injectable({
 	providedIn: "root",
 })
 export class ThemeService {
-	private currentTheme!: Theme;
+	private currentTheme!: BehaviorSubject<Theme>;
 
 	constructor() {
 		afterNextRender(() => {
-			this.currentTheme = (localStorage.getItem("theme") ?? "dark") as Theme;
+			this.currentTheme = new BehaviorSubject(
+				(localStorage.getItem("theme") ?? "dark") as Theme,
+			);
+
+			this.currentTheme.subscribe((theme) => {
+				localStorage.setItem("theme", theme);
+
+				theme === "light"
+					? document.documentElement.classList.remove("dark")
+					: document.documentElement.classList.add("dark");
+			});
 		});
 	}
 
 	public setTheme(theme: Theme) {
-		theme === "light"
-			? document.documentElement.classList.remove("dark")
-			: document.documentElement.classList.add("dark");
-
-		this.currentTheme = theme;
-		localStorage.setItem("theme", this.currentTheme);
+		this.currentTheme.next(theme);
 	}
 
-	public getCurrentTheme(): Theme {
+	public toggleTheme() {
+		const nextTheme: Theme =
+			this.currentTheme.getValue() === "light" ? "dark" : "light";
+		this.currentTheme.next(nextTheme);
+	}
+
+	public getCurrentTheme(): Observable<Theme> {
 		return this.currentTheme;
 	}
 }

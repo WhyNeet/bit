a71c1eb483ef714f1159a6f247ba96abb62c68b7
Yet Another Stupid Coder <117515380+WhyNeet@users.noreply.github.com>
2024-05-10 18:43:59 +0300

    fix(web): use router link in app-logo

--- apps/web/src/app/components/logo/logo.component.ts
+++ apps/web/src/app/components/logo/logo.component.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component } from "@angular/core";
+import { RouterLink } from "@angular/router";
 
 @Component({
 	selector: "app-logo",
 	standalone: true,
-	imports: [],
+	imports: [RouterLink],
 	providers: [],
 	templateUrl: "./logo.component.html",
 	changeDetection: ChangeDetectionStrategy.OnPush,

b6bbe7c76961b57209c99e9f5c0e31526447ac72
Yet Another Stupid Coder <117515380+WhyNeet@users.noreply.github.com>
2024-05-10 21:56:15 +0300

    feat(web): add sign in and popular links to sidebar

--- apps/web/src/app/components/sidebar/sidebar.component.ts
+++ apps/web/src/app/components/sidebar/sidebar.component.ts
@@ -9,15 +9,18 @@ import { NgIcon, provideIcons } from "@ng-icons/core";
 import {
 	lucideBell,
 	lucideCog,
+	lucideFlame,
 	lucideHome,
+	lucideLogIn,
 	lucideMessageCircle,
 } from "@ng-icons/lucide";
 import { Store, select } from "@ngrx/store";
 import { UserDto } from "common";
 import { Observable, map } from "rxjs";
 import { ThemeService } from "../../features/theme/theme.service";
-import { selectUser } from "../../state/user/selectors";
+import { selectIsUserLoading, selectUser } from "../../state/user/selectors";
 import { LogoComponent } from "../logo/logo.component";
+import { SkeletonComponent } from "../ui/skeleton/skeleton.component";
 import { SwitchComponent } from "../ui/switch/switch.component";
 
 @Component({
@@ -30,16 +33,25 @@ import { SwitchComponent } from "../ui/switch/switch.component";
 		NgIcon,
 		CommonModule,
 		SwitchComponent,
+		SkeletonComponent,
 	],
 	providers: [ThemeService],
 	viewProviders: [
-		provideIcons({ lucideHome, lucideMessageCircle, lucideBell, lucideCog }),
+		provideIcons({
+			lucideHome,
+			lucideMessageCircle,
+			lucideBell,
+			lucideCog,
+			lucideLogIn,
+			lucideFlame,
+		}),
 	],
 	templateUrl: "./sidebar.component.html",
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SidebarComponent {
 	protected user$: Observable<UserDto | null>;
+	protected isUserLoading$: Observable<boolean>;
 	protected isDarkTheme$!: Observable<boolean>;
 
 	constructor(
@@ -47,6 +59,7 @@ export class SidebarComponent {
 		private themeService: ThemeService,
 	) {
 		this.user$ = this.store.pipe(select(selectUser));
+		this.isUserLoading$ = this.store.pipe(select(selectIsUserLoading));
 
 		afterNextRender(() => {
 			this.isDarkTheme$ = this.themeService

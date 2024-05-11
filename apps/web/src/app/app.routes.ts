04e33c6d7c290e3c5ef909e6b39e28906ff4a860
Yet Another Stupid Coder <117515380+WhyNeet@users.noreply.github.com>
2024-05-11 13:44:16 +0300

    revert(web): remove auth routes, will use dialog instead

--- apps/web/src/app/app.routes.ts
+++ apps/web/src/app/app.routes.ts
@@ -7,18 +7,6 @@ export const routes: Routes = [
 			import("./routes/home/home.component").then((m) => m.HomeComponent),
 	},
 	{
-		path: "auth",
-		children: [
-			{
-				path: "sign-in",
-				loadComponent: () =>
-					import("./routes/sign-in/sign-in.component").then(
-						(m) => m.SignInComponent,
-					),
-			},
-		],
-	},
-	{
 		path: "",
 		redirectTo: "home",
 		pathMatch: "full",

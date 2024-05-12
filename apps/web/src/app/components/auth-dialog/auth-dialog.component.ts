134d93dcfef8cf390925263339489d57ffb871ea
Yet Another Stupid Coder <117515380+WhyNeet@users.noreply.github.com>
2024-05-12 15:05:54 +0300

    style(web): add tabs change animation

--- apps/web/src/app/components/auth-dialog/auth-dialog.component.ts
+++ apps/web/src/app/components/auth-dialog/auth-dialog.component.ts
@@ -1,16 +1,89 @@
-import { ChangeDetectionStrategy, Component, signal } from "@angular/core";
+import {
+	animate,
+	group,
+	query,
+	style,
+	transition,
+	trigger,
+} from "@angular/animations";
+import {
+	ChangeDetectionStrategy,
+	Component,
+	computed,
+	signal,
+} from "@angular/core";
+import { DialogContent } from "../ui/dialog-container/dialog-content.component";
+import { TabComponent } from "../ui/tabs/tab/tab.component";
+import { TabsComponent } from "../ui/tabs/tabs.component";
 import { AuthDialogLoginComponent } from "./login/auth-dialog-login.component";
-import { AuthDialogSignupComponent } from "./signup/auth-dialog-signup.component";
+import { AuthDialogSignupComponent } from "./register/auth-dialog-register.component";
 
 @Component({
 	selector: "app-dialog-auth",
 	standalone: true,
-	imports: [AuthDialogLoginComponent, AuthDialogSignupComponent],
+	imports: [
+		AuthDialogLoginComponent,
+		AuthDialogSignupComponent,
+		TabsComponent,
+		TabComponent,
+		DialogContent,
+	],
 	providers: [],
+	animations: [
+		trigger("paneChange", [
+			transition(
+				"* => *",
+				[
+					query(":self", [style({ height: "{{startHeight}}px" })]),
+					query(":enter", [
+						style({
+							opacity: 0,
+							transform: "translateX({{enterStart}})",
+						}),
+					]),
+					query(
+						":leave",
+						[
+							style({
+								opacity: 1,
+								position: "absolute",
+								transform: "translateX(0)",
+							}),
+							animate(
+								"0.2s ease-in-out",
+								style({
+									opacity: 0,
+									transform: "translateX({{leaveEnd}})",
+								}),
+							),
+						],
+						{ optional: true },
+					),
+					group(
+						[
+							query(":self", [
+								animate("0.2s ease-in-out", style({ height: "*" })),
+							]),
+							query(":enter", [
+								animate(
+									"0.2s ease-in-out",
+									style({ opacity: 1, transform: "translateX(0)" }),
+								),
+							]),
+						],
+						{ params: { startHeight: 0 } },
+					),
+				],
+				{ params: { enterStart: "", leaveEnd: "" } },
+			),
+		]),
+	],
 	templateUrl: "./auth-dialog.component.html",
 	styleUrl: "./auth-dialog.component.css",
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AuthDialogComponent {
-	protected isLogin = signal(true);
+	protected tab = signal(0);
+	protected enterStart = computed(() => (this.tab() === 1 ? "100%" : "-100%"));
+	protected leaveEnd = computed(() => (this.tab() === 1 ? "-100%" : "100%"));
 }
--- apps/web/src/app/components/auth-dialog/register/auth-dialog-register.component.ts
+++ apps/web/src/app/components/auth-dialog/register/auth-dialog-register.component.ts
@@ -6,7 +6,7 @@ import { ChangeDetectionStrategy, Component } from "@angular/core";
 	imports: [],
 	providers: [],
 	template: `
-
+		register
     `,
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })

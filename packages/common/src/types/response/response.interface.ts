679379b6e6df724f71f7463c6d792bde3b7eecd7
Yet Another Stupid Coder <117515380+WhyNeet@users.noreply.github.com>
2024-05-12 20:18:20 +0300

    feat(common): add error response interface

--- packages/common/src/types/response/response.interface.ts
+++ packages/common/src/types/response/response.interface.ts
@@ -2,4 +2,11 @@ export interface Response<T> {
 	data: T;
 }
 
+export interface ErrorResponse {
+	type: string;
+	title: string;
+	detail: string;
+	instance: string;
+}
+
 export type ApiResponse<T> = Promise<Response<T>>;

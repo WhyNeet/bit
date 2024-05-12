9910f0323f40f13ab7508c6b9febfebe1357a87d
Yet Another Stupid Coder <117515380+WhyNeet@users.noreply.github.com>
2024-05-12 20:17:31 +0300

    feat(common): add constructors for user dtos

--- packages/common/src/dtos/user.dto.ts
+++ packages/common/src/dtos/user.dto.ts
@@ -6,12 +6,24 @@ export class CreateUserDto {
 	password: string;
 
 	name: string;
+
+	constructor(email: string, username: string, password: string, name: string) {
+		this.email = email;
+		this.username = username;
+		this.password = password;
+		this.name = name;
+	}
 }
 
 export class UserCredentialsDto {
 	email: string;
 
 	password: string;
+
+	constructor(email: string, password: string) {
+		this.email = email;
+		this.password = password;
+	}
 }
 
 export class UserDto {

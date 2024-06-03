059f57d3226e22ee92937b2c770e52cb20e4f10d
Yet Another Stupid Coder <117515380+WhyNeet@users.noreply.github.com>
2024-06-03 17:22:58 +0300

    fix(web): show editor only if user is logged in

--- apps/web/src/app/routes/home/home.component.ts
+++ apps/web/src/app/routes/home/home.component.ts
@@ -1,14 +1,26 @@
+import { CommonModule } from "@angular/common";
 import { ChangeDetectionStrategy, Component, HostBinding } from "@angular/core";
+import { Store, select } from "@ngrx/store";
+import { Observable, map } from "rxjs";
 import { PostFormComponent } from "../../components/post-form/post-form.component";
+import { selectUser } from "../../state/user/selectors";
 
 @Component({
 	selector: "app-page-home",
 	standalone: true,
-	imports: [PostFormComponent],
+	imports: [PostFormComponent, CommonModule],
 	providers: [],
 	templateUrl: "./home.component.html",
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomeComponent {
 	@HostBinding() class = "flex-auto";
+	protected isLoggedIn: Observable<boolean>;
+
+	constructor(private store: Store) {
+		this.isLoggedIn = this.store.pipe(
+			select(selectUser),
+			map((user) => !!user),
+		);
+	}
 }

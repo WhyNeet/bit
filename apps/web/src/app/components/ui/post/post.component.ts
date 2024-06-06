8e9315e7f0a53bffda6e7992d36e04e6fca46d0a
Yet Another Stupid Coder <117515380+WhyNeet@users.noreply.github.com>
2024-06-06 19:52:45 +0300

    feat(web): add post component

---
+++ apps/web/src/app/components/ui/post/post.component.ts
@@ -0,0 +1,36 @@
+import {
+	ChangeDetectionStrategy,
+	Component,
+	Input,
+	OnInit,
+} from "@angular/core";
+import { NgIcon, provideIcons } from "@ng-icons/core";
+import { lucideThumbsDown, lucideThumbsUp } from "@ng-icons/lucide";
+import { PostDto, UserDto } from "common";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+import { UserService } from "../../../features/user/user.service";
+import { AvatarComponent } from "../avatar/avatar.component";
+
+dayjs.extend(relativeTime);
+
+@Component({
+	selector: "app-ui-post",
+	standalone: true,
+	imports: [AvatarComponent, NgIcon],
+	providers: [UserService],
+	viewProviders: [provideIcons({ lucideThumbsUp, lucideThumbsDown })],
+	templateUrl: "./post.component.html",
+	changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class PostComponent implements OnInit {
+	constructor(protected userService: UserService) {}
+
+	@Input() post!: PostDto & { author: UserDto };
+
+	protected timeElapsed = "";
+
+	ngOnInit(): void {
+		this.timeElapsed = dayjs(this.post.createdAt).fromNow();
+	}
+}

b08e73d37cf9bab51ed095951512e94db5b7b73d
Yet Another Stupid Coder <117515380+WhyNeet@users.noreply.github.com>
2024-06-08 22:52:54 +0300

    feat(web): add community service

---
+++ apps/web/src/app/features/community/community.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from "@angular/core";
+import { environment } from "../../../environments/environment";
+
+@Injectable({
+	providedIn: "root",
+})
+export class CommunityService {
+	public getCommunityIconUrl(id: string): string {
+		return `${environment.API_BASE_URL}/media/community/${id}/icon`;
+	}
+}

6600d6450670b1c008add4db3dd1bedd77d6af00
Yet Another Stupid Coder <117515380+WhyNeet@users.noreply.github.com>
2024-06-09 20:44:45 +0300

    perf(web): add compression to improve loading speed

--- apps/web/server.ts
+++ apps/web/server.ts
@@ -2,12 +2,14 @@ import { dirname, join, resolve } from "node:path";
 import { fileURLToPath } from "node:url";
 import { APP_BASE_HREF } from "@angular/common";
 import { CommonEngine } from "@angular/ssr";
+import compression from "compression";
 import express from "express";
 import bootstrap from "./src/main.server";
 
 // The Express app is exported so that it can be used by serverless Functions.
 export function app(): express.Express {
 	const server = express();
+	server.use(compression());
 	const serverDistFolder = dirname(fileURLToPath(import.meta.url));
 	const browserDistFolder = resolve(serverDistFolder, "../browser");
 	const indexHtml = join(serverDistFolder, "index.server.html");

72ecd4daca701621766097e54c8bd18a4966815e
Yet Another Stupid Coder <117515380+WhyNeet@users.noreply.github.com>
2024-06-09 20:39:09 +0300

    feat(web): add optional custom icon in avatar

--- apps/web/src/app/app.config.ts
+++ apps/web/src/app/app.config.ts
@@ -7,6 +7,7 @@ import {
 	withHttpTransferCacheOptions,
 } from "@angular/platform-browser";
 import { provideAnimations } from "@angular/platform-browser/animations";
+import { provideNgIconLoader, withCaching } from "@ng-icons/core";
 import { provideStore } from "@ngrx/store";
 import { provideStoreDevtools } from "@ngrx/store-devtools";
 import { routes } from "./app.routes";
@@ -24,5 +25,8 @@ export const appConfig: ApplicationConfig = {
 		provideHttpClient(),
 		provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }),
 		provideAnimations(),
+		provideNgIconLoader(async (name) => {
+			return (await import("@ng-icons/lucide"))[name] as string;
+		}, withCaching()),
 	],
 };
--- apps/web/src/app/components/ui/avatar/avatar.component.ts
+++ apps/web/src/app/components/ui/avatar/avatar.component.ts
@@ -2,10 +2,12 @@ import { NgOptimizedImage } from "@angular/common";
 import {
 	ChangeDetectionStrategy,
 	Component,
+	OnInit,
 	computed,
 	input,
+	signal,
 } from "@angular/core";
-import { NgIcon, provideIcons } from "@ng-icons/core";
+import { NgIcon, injectNgIconLoader, provideIcons } from "@ng-icons/core";
 import { lucideUserRound } from "@ng-icons/lucide";
 
 @Component({
@@ -16,16 +18,35 @@ import { lucideUserRound } from "@ng-icons/lucide";
 	viewProviders: [provideIcons({ lucideUserRound })],
 	template: `
   <div [style]="{ height: (size() ?? 24) + 'px' }" class="relative flex items-center justify-center aspect-square rounded-full {{ withBackground() ? 'bg-text/5' : '' }}">
-    <ng-icon size="24" name="lucideUserRound" />
+    @if (customIcon()) {
+      <ng-icon size="24" [svg]="customSvg()" />
+    } @else {
+      <ng-icon size="24" name="lucideUserRound" />
+    }
     <img [ngSrc]="src()" class="absolute inset-0 rounded-full" [height]="iconSize()" [width]="iconSize()" onerror="this.style.display='none'" />
   </div>
   `,
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AvatarComponent {
+export class AvatarComponent implements OnInit {
 	src = input.required<string>();
 	size = input<number>();
 	withBackground = input<boolean>();
+	customIcon = input<string>();
+
+	// biome-ignore lint/style/noNonNullAssertion: provided in app.config.ts
+	private iconLoader = injectNgIconLoader()!;
+
+	// biome-ignore lint/style/noNonNullAssertion: set in lifecycle hook
+	protected customSvg = signal<string>(null!);
+
+	async ngOnInit() {
+		if (this.customIcon()) {
+			// biome-ignore lint/style/noNonNullAssertion: the return value is string
+			const icon = (await this.iconLoader(this.customIcon()!)) as string;
+			this.customSvg.set(icon);
+		}
+	}
 
 	protected iconSize = computed(() => {
 		return (this.size() ?? 24).toString();

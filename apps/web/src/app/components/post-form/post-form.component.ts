1027a426efa776a3e4cdb1e2e4584d3ab5225143
Yet Another Stupid Coder <117515380+WhyNeet@users.noreply.github.com>
2024-06-13 22:11:33 +0300

    fix(web): make publisher dropdown in post-form work

--- apps/web/src/app/components/post-form/post-form.component.ts
+++ apps/web/src/app/components/post-form/post-form.component.ts
@@ -2,19 +2,20 @@ import { CdkTextareaAutosize } from "@angular/cdk/text-field";
 import { CommonModule } from "@angular/common";
 import {
 	ChangeDetectionStrategy,
-	ChangeDetectorRef,
 	Component,
-	afterNextRender,
+	Signal,
+	WritableSignal,
 	signal,
 } from "@angular/core";
+import { toSignal } from "@angular/core/rxjs-interop";
 import { FormControl, ReactiveFormsModule, Validators } from "@angular/forms";
 import { Store, select } from "@ngrx/store";
-import { CommunityDto } from "common";
+import { CommunityDto, UserDto } from "common";
 import { Observable, map, take, takeWhile } from "rxjs";
 import { CommunityService } from "../../features/community/community.service";
 import { PostsService } from "../../features/posts/posts.service";
 import { UserService } from "../../features/user/user.service";
-import { selectUserCommunities } from "../../state/user/selectors";
+import { selectUser, selectUserCommunities } from "../../state/user/selectors";
 import { EditorComponent } from "../editor/editor.component";
 import {
 	DropdownComponent,
@@ -36,8 +37,8 @@ import {
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PostFormComponent {
-	private userCommunities$!: Observable<CommunityDto[] | null>;
-	protected publishOptions$!: Observable<DropdownItem[]>;
+	private userCommunities = signal<CommunityDto[]>([]);
+	protected publishOptions = signal<DropdownItem[]>([]);
 	protected title = new FormControl("", {
 		validators: [
 			Validators.required,
@@ -45,38 +46,61 @@ export class PostFormComponent {
 			Validators.maxLength(255),
 		],
 	});
-	private community = signal<string | null>(null);
+	private communityId = signal<string | null>(null);
+	private currentUser!: Signal<UserDto>;
 
 	constructor(
 		private store: Store,
 		private userService: UserService,
 		private postsService: PostsService,
 		private communityService: CommunityService,
-		private cdr: ChangeDetectorRef,
 	) {
-		afterNextRender(() => {
-			this.userCommunities$ = this.store.pipe(select(selectUserCommunities));
-			this.userCommunities$
-				.pipe(
-					takeWhile((communities) => !communities),
-					take(1),
-				)
-				.subscribe(() => this.userService.getCurrentUserCommunities());
+		this.currentUser = toSignal(
+			this.store.pipe(select(selectUser)),
+		) as Signal<UserDto>;
 
-			this.publishOptions$ = this.userCommunities$.pipe(
+		const userCommunities = this.store.pipe(select(selectUserCommunities));
+
+		this.userCommunities = toSignal(userCommunities) as WritableSignal<
+			CommunityDto[]
+		>;
+		userCommunities
+			.pipe(
+				takeWhile((communities) => !communities),
+				take(1),
+			)
+			.subscribe(() => this.userService.getCurrentUserCommunities());
+
+		this.publishOptions = toSignal(
+			userCommunities.pipe(
 				map((communities) =>
 					(communities ?? []).map(
 						(c) =>
 							({
 								label: c.name,
 								imageUrl: this.communityService.getCommunityIconUrl(c.id),
+								icon: "lucideUsersRound",
 							}) as DropdownItem,
 					),
 				),
-			);
+				map((communities) => [
+					{
+						label: this.currentUser().username,
+						imageUrl: this.userService.getCurrentUserAvatarUrl(),
+						icon: "lucideUserRound",
+					},
+					...communities,
+				]),
+			),
+		) as WritableSignal<DropdownItem[]>;
+	}
 
-			this.publishOptions$.subscribe(() => this.cdr.detectChanges());
-		});
+	protected onPublisherSelectionChange(idx: number) {
+		// first publisher is current user, thus the community is none
+		const communityId =
+			idx === 0 ? null : (this.userCommunities().at(idx - 1)?.id as string);
+
+		this.communityId.set(communityId);
 	}
 
 	protected handleSend(content: string) {
@@ -86,7 +110,7 @@ export class PostFormComponent {
 			content,
 			[],
 			[],
-			this.community() ?? undefined,
+			this.communityId() ?? undefined,
 		);
 	}
 }

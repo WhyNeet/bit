4792f861d60f0746e606397159068c72d666cfb2
Yet Another Stupid Coder <117515380+WhyNeet@users.noreply.github.com>
2024-06-14 22:11:41 +0300

    feat(web): record search history on successful queries

--- apps/web/src/app/features/search/search.service.ts
+++ apps/web/src/app/features/search/search.service.ts
@@ -2,18 +2,22 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Store } from "@ngrx/store";
 import { PostVectorData } from "common";
-import { catchError, map, throwError } from "rxjs";
+import { catchError, map, tap, throwError } from "rxjs";
 import { environment } from "../../../environments/environment";
-import { searchFinished } from "../../state/search/actions";
+import { searchFinished, searchLoading } from "../../state/search/actions";
+import { LocalStorageService } from "../storage/local-storage.service";
 
 @Injectable({ providedIn: "root" })
 export class SearchService {
 	constructor(
 		private httpClient: HttpClient,
 		private store: Store,
+		private localStorageService: LocalStorageService,
 	) {}
 
 	public search(query: string) {
+		this.store.dispatch(searchLoading());
+
 		this.httpClient
 			.get(`${environment.API_BASE_URL}/posts/search?query=${query}`)
 			.pipe(
@@ -21,9 +25,17 @@ export class SearchService {
 				catchError((err) => {
 					return throwError(() => err);
 				}),
+				tap(() => this.saveToHistory(query)),
 			)
 			.subscribe((results) =>
 				this.store.dispatch(searchFinished({ posts: results })),
 			);
+	}
+
+	private saveToHistory(query: string) {
+		const storedHistory = this.localStorageService.getItem("searchHistory");
+		const history: string[] = storedHistory ? JSON.parse(storedHistory) : [];
+
+		this.localStorageService.setItem("searchHistory", [query, ...history]);
 	}
 }

8b7d7b72e0a139204ded8b2e37d8ed42a50e2845
Yet Another Stupid Coder <117515380+WhyNeet@users.noreply.github.com>
2024-06-14 18:32:52 +0300

    refactor(web): use dialog service in auth-button component

--- apps/web/src/app/components/auth-button/auth-button.component.ts
+++ apps/web/src/app/components/auth-button/auth-button.component.ts
@@ -1,8 +1,6 @@
-import { Dialog } from "@angular/cdk/dialog";
 import { ChangeDetectionStrategy, Component, Input } from "@angular/core";
+import { DialogService } from "../../features/dialog/dialog.service";
 import { AuthDialogComponent } from "../auth-dialog/auth-dialog.component";
-import { AnimatedDialogRef } from "../ui/dialog-container/dialog-animated-ref";
-import { DialogContainerComponent } from "../ui/dialog-container/dialog-container.component";
 
 @Component({
 	selector: "app-auth-button",
@@ -19,24 +17,9 @@ import { DialogContainerComponent } from "../ui/dialog-container/dialog-containe
 export class AuthButtonComponent {
 	@Input() class = "";
 
-	constructor(private dialog: Dialog) {}
+	constructor(private dialogService: DialogService) {}
 
 	protected openAuthDialog() {
-		let animatedDialogRef: AnimatedDialogRef<unknown>;
-
-		const ref = this.dialog.open<unknown>(AuthDialogComponent, {
-			container: DialogContainerComponent,
-			disableClose: true,
-			closeOnOverlayDetachments: true,
-			providers(dialogRef, config, container) {
-				animatedDialogRef = new AnimatedDialogRef(dialogRef);
-				return [
-					{
-						provide: AnimatedDialogRef,
-						useValue: animatedDialogRef,
-					},
-				];
-			},
-		});
+		this.dialogService.open(AuthDialogComponent);
 	}
 }

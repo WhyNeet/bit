2f1555767fd1f3120c1d262f22b28bcf008bc9fb
Yet Another Stupid Coder <117515380+WhyNeet@users.noreply.github.com>
2024-06-14 18:29:42 +0300

    feat(web): add dialog service

---
+++ apps/web/src/app/features/dialog/dialog.service.ts
@@ -0,0 +1,29 @@
+import { Dialog } from "@angular/cdk/dialog";
+import { ComponentType } from "@angular/cdk/portal";
+import { Injectable } from "@angular/core";
+import { AnimatedDialogRef } from "../../components/ui/dialog-container/dialog-animated-ref";
+import { DialogContainerComponent } from "../../components/ui/dialog-container/dialog-container.component";
+
+@Injectable({ providedIn: "root" })
+export class DialogService {
+	constructor(private dialog: Dialog) {}
+
+	public open(component: ComponentType<unknown>) {
+		let animatedDialogRef: AnimatedDialogRef<unknown>;
+
+		const ref = this.dialog.open<unknown>(component, {
+			container: DialogContainerComponent,
+			disableClose: true,
+			closeOnOverlayDetachments: true,
+			providers(dialogRef, _config, _container) {
+				animatedDialogRef = new AnimatedDialogRef(dialogRef);
+				return [
+					{
+						provide: AnimatedDialogRef,
+						useValue: animatedDialogRef,
+					},
+				];
+			},
+		});
+	}
+}

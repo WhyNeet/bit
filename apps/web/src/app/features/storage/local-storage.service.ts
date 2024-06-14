86ad1f883c27131cb5865a218e903e62e206dd35
Yet Another Stupid Coder <117515380+WhyNeet@users.noreply.github.com>
2024-06-14 22:00:06 +0300

    feat(web): add localStorage service

---
+++ apps/web/src/app/features/storage/local-storage.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from "@angular/core";
+import { ReplaySubject } from "rxjs";
+
+@Injectable({ providedIn: "root" })
+export class LocalStorageService {
+	private storageChange$ = new ReplaySubject<{ key: string; value: unknown }>();
+
+	// biome-ignore lint/complexity/noBannedTypes: Object represents a random object
+	public setItem(key: string, value: string | Object) {
+		const serializedValue =
+			typeof value === "string" ? value : JSON.stringify(value);
+
+		window.localStorage.setItem(key, serializedValue);
+		this.storageChange$.next({ key, value });
+	}
+
+	public getItem(key: string) {
+		return window.localStorage.getItem(key);
+	}
+
+	public getChanges() {
+		return this.storageChange$.asObservable();
+	}
+}

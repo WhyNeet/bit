108bbd41934ee9cb57eb46d0125e410ddb80e7f2
Yet Another Stupid Coder <117515380+WhyNeet@users.noreply.github.com>
2024-06-14 17:32:43 +0300

    feat(web): add kbd ui component

---
+++ apps/web/src/app/components/ui/kbd/kbd.component.ts
@@ -0,0 +1,15 @@
+import { ChangeDetectionStrategy, Component } from "@angular/core";
+
+@Component({
+	selector: "app-ui-kdb",
+	standalone: true,
+	imports: [],
+	providers: [],
+	template: `
+	  <div class="px-1.5 py-0.5 bg-text/10 rounded-[calc(var(--border-radius)*0.8)] text-text/40 text-sm font-display flex items-center gap-1">
+			<ng-content />
+		</div>
+	`,
+	changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class KBDComponent {}

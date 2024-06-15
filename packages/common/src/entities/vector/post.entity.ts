696834ec939e405a2f5ce582e2009a817034688a
Yet Another Stupid Coder <117515380+WhyNeet@users.noreply.github.com>
2024-06-15 18:23:48 +0300

    feat(common): add post vector data dto

--- packages/common/src/entities/vector/post.entity.ts
+++ packages/common/src/entities/vector/post.entity.ts
@@ -1,7 +1,17 @@
+import { CommunityDto, UserDto } from "src/dtos";
+
 export class PostVectorData {
 	id: string;
 	title: string;
 	vector: number[];
 	author: string;
 	community: string | undefined;
+}
+
+export class PostVectorDataDto {
+	id: string;
+	title: string;
+	vector: number[];
+	author: string | UserDto;
+	community: string | CommunityDto | undefined;
 }

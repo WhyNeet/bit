3fec0f1e9ebd895e9a1cc12a22fd1a86f4ab7cef
Yet Another Stupid Coder <117515380+WhyNeet@users.noreply.github.com>
2024-06-15 18:40:18 +0300

    fix(common): change author and community to entities from dtos in post vector data

--- packages/common/src/entities/vector/post.entity.ts
+++ packages/common/src/entities/vector/post.entity.ts
@@ -1,11 +1,13 @@
 import { CommunityDto, UserDto } from "src/dtos";
+import { Community } from "../community.entity";
+import { User } from "../user.entity";
 
 export class PostVectorData {
 	id: string;
 	title: string;
 	vector: number[];
-	author: string | UserDto;
-	community: string | CommunityDto | undefined;
+	author: string | User;
+	community: string | Community | undefined;
 }
 
 export class PostVectorDataDto {

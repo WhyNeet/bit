c40e8e1109afb429d171492e53a252338b754d97
Yet Another Stupid Coder <117515380+WhyNeet@users.noreply.github.com>
2024-06-20 16:05:41 +0300

    feat(api): add post comments endpoint

--- apps/api/src/controllers/comment.controller.ts
+++ apps/api/src/controllers/comment.controller.ts
@@ -1,8 +1,10 @@
 import {
   Body,
   Controller,
+  Get,
   HttpCode,
   HttpStatus,
+  Param,
   Post,
   UseGuards,
 } from "@nestjs/common";
@@ -11,6 +13,7 @@ import { CommentDto } from "common";
 import { CreateCommentDto } from "src/core/dtos/comment.dto";
 import { CommentFactoryService } from "src/features/comment/comment-factory.service";
 import { CommentRepositoryService } from "src/features/comment/comment-repository.service";
+import { ParseObjectIdPipe } from "src/features/pipes/parse-objectid.pipe";
 import { Token } from "src/frameworks/auth/decorators/token.decorator";
 import { JwtAuthGuard } from "src/frameworks/auth/guards/jwt.guard";
 import { JwtPayload } from "src/frameworks/auth/jwt/types/payload.interface";
@@ -35,6 +38,21 @@ export class CommentController {
 
     return {
       data: this.commentFactoryService.createDto(comment),
+    };
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Get("/post/:postId")
+  public async getPostComments(
+    @Param("postId", ParseObjectIdPipe.stringified()) postId: string,
+  ): ApiResponse<CommentDto[]> {
+    const comments =
+      await this.commentRespositoryService.getPostComments(postId);
+
+    return {
+      data: comments.map(
+        this.commentFactoryService.createDto.bind(this.commentFactoryService),
+      ),
     };
   }
 }

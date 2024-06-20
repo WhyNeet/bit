038a3b1ad1703dbf7ff66fba6e31f0fbc555d132
Yet Another Stupid Coder <117515380+WhyNeet@users.noreply.github.com>
2024-06-20 15:14:00 +0300

    feat(common): add comment create and update dtos

--- packages/common/src/dtos/comment.dto.ts
+++ packages/common/src/dtos/comment.dto.ts
@@ -1,6 +1,16 @@
 import { PostDto } from "./post.dto";
 import { UserDto } from "./user.dto";
 
+export class CreateCommentDto {
+  content: string;
+
+  post: string;
+}
+
+export class UpdateCommentDto {
+  content: string;
+}
+
 export class CommentDto {
   id: string;
 

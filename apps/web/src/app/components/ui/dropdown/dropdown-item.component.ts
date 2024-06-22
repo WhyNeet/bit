d42daed553c17c9d55ab701bfd3f62cc4518a12f
Yet Another Stupid Coder <117515380+WhyNeet@users.noreply.github.com>
2024-06-22 12:21:00 +0300

    feat(web): add options component

--- apps/web/src/app/components/ui/dropdown/dropdown-item.component.ts
+++ apps/web/src/app/components/ui/dropdown/dropdown-item.component.ts
@@ -7,7 +7,7 @@ import { ChangeDetectionStrategy, Component, output } from "@angular/core";
   providers: [],
   template: `
     <button (click)="onSelect.emit()" class="flex items-center gap-2 px-2 py-1 rounded-default hover:bg-text/5 w-full">
-			<div class="h-6 w-6 rounded-full overflow-hidden">
+			<div class="h-6 w-6 rounded-full overflow-hidden flex items-center justify-center">
 				<ng-content select="[icon]" />
 			</div>
 			<div class="font-display">
---
+++ apps/web/src/app/components/ui/options/options.component.ts
@@ -0,0 +1,62 @@
+import { CdkConnectedOverlay, CdkOverlayOrigin } from "@angular/cdk/overlay";
+import {
+  ChangeDetectionStrategy,
+  Component,
+  input,
+  output,
+  signal,
+} from "@angular/core";
+import { NgIcon, injectNgIconLoader, provideIcons } from "@ng-icons/core";
+import { lucideMoreVertical } from "@ng-icons/lucide";
+import { DropdownItemComponent } from "../dropdown/dropdown-item.component";
+
+export type Option = {
+  label: string;
+  icon: string;
+};
+
+@Component({
+  selector: "app-ui-options",
+  standalone: true,
+  imports: [
+    CdkConnectedOverlay,
+    CdkOverlayOrigin,
+    NgIcon,
+    DropdownItemComponent,
+  ],
+  providers: [],
+  template: `
+  <button cdkOverlayOrigin #trigger="cdkOverlayOrigin" class="flex items-center gap-2 rounded-default bg-text/5 border border-text/10 p-2 font-display" (click)="toggle()">
+    <ng-icon name="lucideMoreVertical" />
+  </button>
+  <ng-template cdkConnectedOverlay [cdkConnectedOverlayOrigin]="trigger" [cdkConnectedOverlayOpen]="isOpen()" (backdropClick)="isOpen.set(false)" [cdkConnectedOverlayHasBackdrop]="true" cdkConnectedOverlayBackdropClass="!opacity-0">
+      <div class="p-1 bg-text/5 backdrop-blur rounded-default border border-text/10 min-w-48 translate-y-28">
+          @for (item of options(); track item.label) {
+            <app-ui-dropdown-item (onSelect)="setCurrentItem($index)">
+                <ng-icon [name]="item.icon" size="18" icon />
+                {{ item.label }}
+            </app-ui-dropdown-item>
+          }
+      </div>
+  </ng-template>
+  `,
+  viewProviders: [provideIcons({ lucideMoreVertical })],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class OptionsComponent {
+  options = input.required<Option[]>();
+  protected isOpen = signal(false);
+
+  onSelectionChange = output<number>();
+
+  protected iconLoader = injectNgIconLoader() as (name: string) => string;
+
+  protected setCurrentItem(idx: number) {
+    this.isOpen.set(false);
+    this.onSelectionChange.emit(idx);
+  }
+
+  protected toggle() {
+    this.isOpen.update((prev) => !prev);
+  }
+}

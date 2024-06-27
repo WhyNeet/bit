476d632c9e00f7d7ce03f8fd62ce36a89bd70800
Yet Another Stupid Coder <117515380+WhyNeet@users.noreply.github.com>
2024-06-27 16:43:59 +0300

    refactor(api): swap fileid validation logic with a validator from common

--- apps/api/src/features/pipes/parse-fileid.pipe.ts
+++ apps/api/src/features/pipes/parse-fileid.pipe.ts
@@ -1,25 +1,12 @@
 import { ArgumentMetadata, ParseUUIDPipe } from "@nestjs/common";
+import { validateFileId } from "common";
 
 export class ParseFileIdPipe extends ParseUUIDPipe {
   async transform(value: string, metadata: ArgumentMetadata): Promise<string> {
-    if (value.length < 36)
-      throw this.exceptionFactory(
-        `Invalid file identifier length: ${value.length}.`,
-      );
+    const validationResult = validateFileId(value);
 
-    const uuid = value.slice(0, 36);
-    super.transform(uuid, metadata);
-
-    const metadataSegment = value.slice(36);
-    const filenameLengthEnd = metadataSegment.indexOf(".");
-    if (filenameLengthEnd < 1)
-      throw this.exceptionFactory("Invalid file metadata segment.");
-
-    const filenameLength = +metadataSegment.slice(0, filenameLengthEnd);
-    if (Number.isNaN(filenameLength))
-      throw this.exceptionFactory("Invalid file metadata segment.");
-    if (filenameLengthEnd + filenameLength + 1 > metadataSegment.length)
-      throw this.exceptionFactory("Invalid file metadata segment.");
+    if (!validationResult || !this.isUUID(validationResult.uuid))
+      throw this.exceptionFactory("Invalid file identifier.");
 
     return value;
   }

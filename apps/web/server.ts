3a173d42485bd19d1bb48c78c67f29f975b03698
WhyNeet <117515380+WhyNeet@users.noreply.github.com>
2024-07-15 20:59:39 +0300

    refactor(web): listen on 0.0.0.0 if PORT variable is set

--- apps/web/server.ts
+++ apps/web/server.ts
@@ -49,12 +49,13 @@ export function app(): express.Express {
 }
 
 function run(): void {
-  const port = process.env["PORT"] || 4200;
+  const port = +(process.env["PORT"] || 4200);
+  const host = process.env["PORT"] ? "0.0.0.0" : "127.0.0.1";
 
   // Start up the Node server
   const server = app();
-  server.listen(port, () => {
-    console.log(`Node Express server listening on http://localhost:${port}`);
+  server.listen(port, host, () => {
+    console.log(`Node Express server listening on http://${host}:${port}`);
   });
 }
 

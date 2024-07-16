766e63e8973b3d3551072799c565d293da559c1a
WhyNeet <117515380+WhyNeet@users.noreply.github.com>
2024-07-16 20:06:47 +0300

    fix(api): run the server on localhost only if PORT is not set

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -4,6 +4,9 @@ import { ApplicationFactory } from "src/application/application.factory";
 async function bootstrap() {
   const app = await ApplicationFactory.create(AppModule);
 
-  await app.listen(process.env.PORT ?? 8080);
+  await app.listen(
+    process.env.PORT ?? 8080,
+    process.env.PORT ? "0.0.0.0" : "127.0.0.1",
+  );
 }
 bootstrap();

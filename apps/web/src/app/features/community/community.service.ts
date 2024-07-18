3a2677b3e3b06862628634efad4d02ea88d1f83f
WhyNeet <117515380+WhyNeet@users.noreply.github.com>
2024-07-18 19:55:38 +0300

    feat(web): add community data retrieval logic

--- apps/web/src/app/features/community/community.service.ts
+++ apps/web/src/app/features/community/community.service.ts
@@ -1,11 +1,27 @@
+import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { CommunityDto } from "common";
+import { Observable, catchError, map, throwError } from "rxjs";
 import { environment } from "../../../environments/environment";
 
 @Injectable({
   providedIn: "root",
 })
 export class CommunityService {
+  constructor(private httpClient: HttpClient) {}
+
   public getCommunityIconUrl(id: string): string {
     return `${environment.API_BASE_URL}/media/community/${id}/icon`;
+  }
+
+  public getCommunity(id: string): Observable<CommunityDto> {
+    return this.httpClient
+      .get(`${environment.API_BASE_URL}/community/${id}`)
+      .pipe(
+        map((res) => (res as { data: CommunityDto }).data),
+        catchError((err) => {
+          return throwError(() => err);
+        }),
+      );
   }
 }
